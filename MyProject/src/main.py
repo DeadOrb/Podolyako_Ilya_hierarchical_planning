d6f6ec6a9413f012c36803196918a7f6fa17096f
DeadOrb <podoly.ilya1999@yandex.ru>
2019-02-19 20:54:09 +0300

    Незначительные изменения

--- MyProject/src/main.py
+++ MyProject/src/main.py
@@ -1,19 +1,25 @@
+# Для того чтобы код нормально заработал, вам потребуется файл домена в формате .pdll и файл таски в таком же формате.
+# Если название домена не будет совпадать с требующимся у таски, программа выкинет ошибку.
 import sys
 import logging
 
 
 def __main__():
-    if (len(sys.argv) != 3):
+    # TODO: сделать проверку на совместимсоть таски и домена
+    # Проверка корректности параметров задачи
+    if len(sys.argv) != 3:
         logging.error("Неверное количество аргументов в параметрах!")
         print(len(sys.argv))
         exit(1)
 
+    # Загрузка домена
     try:
         domain = open(sys.argv[1], 'r').read()
     except FileNotFoundError:
         logging.error("Неверный путь к домену!")
         exit(2)
 
+    # Загрузка таски
     try:
         task = open(sys.argv[2]).read()
     except FileNotFoundError:
